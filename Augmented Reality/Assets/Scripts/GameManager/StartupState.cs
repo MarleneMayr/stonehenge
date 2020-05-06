4894c7511522112e30979542ffc6fcc43db8df0d
Marlene Mayr <marlene-mayr@live.com>
2020-05-06 12:19:02 +0300

    fixed state machine initialization order

--- Augmented Reality/Assets/Scripts/GameManager/StartupState.cs
+++ Augmented Reality/Assets/Scripts/GameManager/StartupState.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Transactions;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class StartupState : State
 {
