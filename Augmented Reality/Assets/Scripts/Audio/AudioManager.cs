ab557aea1dd059eba356cc5668727bdd62373c75
Eric <eric.thalhammer@gmx.net>
2020-05-10 15:49:11 +0200

    add audio files, set up audio scripts

---
+++ Augmented Reality/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public enum GlobalSounds
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlaySound(GlobalSounds sound)
+    {
+
+    }
+}
---
+++ Augmented Reality/Assets/Scripts/Audio/ImpactSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    [SerializeField] AudioClip sound;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            float volumeScale = Mathf.Max(collision.relativeVelocity.magnitude / 10.0f, 1f);
+            audioSource.PlayOneShot(sound, volumeScale);
+            // TODO vary impact sounds and pitch
+            // use different sound depending on material (floor/brick)
+            // maybe: play sound only on one brick if two bricks collide
+            // play in audiomanager or here?0
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            float volumeScale = Mathf.Max(collision.relativeVelocity.magnitude / 10.0f, 1f);
+            audioSource.PlayOneShot(sound, volumeScale);
+            // TODO vary impact sounds and pitch
+        }
+    }
+}
